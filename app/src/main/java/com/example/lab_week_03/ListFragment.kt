fb41db006ef73850b8686359f8e870f22b9375b8
hanabisparkle <qing2005.el@gmail.com>
2025-09-21 11:30:01 +0700

    Commit no. 2 - add coffee list & detail fragment

--- app/src/main/java/com/example/lab_week_03/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_03/ListFragment.kt
@@ -2,7 +2,6 @@ package com.example.lab_week_03
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,17 +14,26 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
+ * Use the [ListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainFragment : Fragment() {
+
+
+class ListFragment : Fragment(), View.OnClickListener{
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onAttach(context: Context){
+    private lateinit var coffeeListener: CoffeeListener;
+
+    override fun onAttach(context: Context) {
         super.onAttach(context)
-        Log.d(TAG, "onAttach")
+        if(context is CoffeeListener){
+            coffeeListener = context
+        }
+        else {
+            throw RuntimeException("Must implement CoffeeListener")
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +42,6 @@ class MainFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-        Log.d(TAG, "onCreate")
     }
 
     override fun onCreateView(
@@ -42,34 +49,27 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        Log.d(TAG, "onCreateView")
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(TAG, "onViewCreated")
+        val coffeeList = listOf<View>(
+            view.findViewById(R.id.affogato),
+            view.findViewById(R.id.americano),
+            view.findViewById(R.id.latte)
+        )
+
+        coffeeList.forEach{
+            it.setOnClickListener(this)
+        }
     }
 
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        Log.d(TAG, "onDestroyView")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-    override fun onDetach() {
-        super.onDetach()
-        Log.d(TAG, "onDetach")
+
+    override fun onClick(v: View?){
+        val let = v?.let { coffee ->
+            coffeeListener.onSelected(coffee.id)
+        }
     }
 
     companion object {
@@ -79,17 +79,16 @@ class MainFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
+         * @return A new instance of fragment ListFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
+            ListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-        private const val TAG = "MainFragment"
     }
 }
